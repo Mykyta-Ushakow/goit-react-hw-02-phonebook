08c614cffd5936df29e68a9cb6a16c712003b904
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-11-15 00:51:08 +0200

    Done

--- src/components/ContactForm/ContactList.jsx
+++ src/components/ContactForm/ContactList.jsx
@@ -5,14 +5,18 @@ export class ContactList extends Component {
   render() {
     return (
       <ul>
-        {this.props.initialValues.map(contact => (
-          <Contact
-            name={contact.name}
-            number={contact.number}
-            key={contact.id}
-            handleDelete={this.props.handleDelete}
-          />
-        ))}
+        {this.props.initialValues.length !== 0 ? (
+          this.props.initialValues.map(contact => (
+            <Contact
+              name={contact.name}
+              number={contact.number}
+              key={contact.id}
+              handleDelete={this.props.handleDelete}
+            />
+          ))
+        ) : (
+          <p>No contacts saved</p>
+        )}
       </ul>
     );
   }
